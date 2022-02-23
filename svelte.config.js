1c9a98eeefa8f1244403cc08df25a641999c6863
ShermanZero <15kpsherman@gmail.com>
2022-02-23 08:55:08 -0500

    following cloudfare tutorial

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
-export default {
-	kit: {
-		adapter: adapter()
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  }
 };
+
+export default config;
