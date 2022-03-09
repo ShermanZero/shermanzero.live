e8ef82c5a8d8349a2c90f348839569e9ef2393e5
ShermanZero <15kpsherman@gmail.com>
2022-03-08 22:14:40 -0500

    v0.0.2.4

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,61 +1,92 @@
 <script>
-  $: hover = null;
-
   $: lastUpdated = null;
   $: lastMessage = null;
+
+  $: icons = true;
 
   const fetchLastUpdate = () => {
     fetch("https://api.github.com/repos/shermanzero/shermanzero.com/branches/main")
       .then((res) => res.json())
       .then((data) => {
-        lastUpdated = data.commit.commit.committer.date;
-        lastUpdated = new Date(lastUpdated).toLocaleString();
-        lastMessage = data.commit.commit.message;
+        if (data.commit) {
+          lastUpdated = data.commit.commit.committer.date;
+          lastUpdated = new Date(lastUpdated).toLocaleString();
+          lastMessage = data.commit.commit.message;
+        }
       });
   };
 
-  const onMouseEnter = (e) => {
-    hover = e.target.innerHTML.trim().toLowerCase();
-  };
-
-  const onMouseLeave = (e) => {
-    hover = null;
+  const toggleIcons = () => {
+    icons = !icons;
   };
 
   fetchLastUpdate();
 </script>
 
-<footer class="update">
-  <a href="https://github.com/ShermanZero/shermanzero.com" target="_">LATEST BUILD</a> ON [{lastUpdated}] -
-  <q>{lastMessage}</q>
-</footer>
 <nav>
-  <div>
-    <span
-      ><a href="/"
-        >shermanzero.live/
-        {#if hover}
-          <span id="link">{hover}</span>
-        {/if}
-      </a></span
-    >
+  <div class="wrapper">
+    <span><a href="/">shermanzero.live</a></span>
+
     <ul id="navlist">
-      <li><a href="/api" on:mouseenter={onMouseEnter} on:mouseleave={onMouseLeave}>API</a></li>
-      <li><a href="/downloads" on:mouseenter={onMouseEnter} on:mouseleave={onMouseLeave}>Downloads</a></li>
-      <li><a href="/portfolio" on:mouseenter={onMouseEnter} on:mouseleave={onMouseLeave}>Portfolio</a></li>
-      <li><a href="/projects" on:mouseenter={onMouseEnter} on:mouseleave={onMouseLeave}>Projects</a></li>
-      <li><a href="/upcoming" on:mouseenter={onMouseEnter} on:mouseleave={onMouseLeave}>Upcoming</a></li>
+      <li class="toggle">
+        <label class="switch">
+          <input type="checkbox" checked on:change={toggleIcons} />
+          <span class="slider round" />
+        </label>
+      </li>
+      {#if icons}
+        <li>
+          <a href="/api" class="icon"> <img src="/svgs/nav/api.svg" width="28" alt="api" /> </a>
+        </li>
+        <li>
+          <a href="/downloads" class="icon"> <img src="/svgs/nav/downloads.svg" width="28" alt="downloads" /> </a>
+        </li>
+        <li>
+          <a href="/portfolio" class="icon"> <img src="/svgs/nav/portfolio.svg" width="28" alt="portfolio" /> </a>
+        </li>
+        <li>
+          <a href="/projects" class="icon"> <img src="/svgs/nav/projects.svg" width="28" alt="projects" /> </a>
+        </li>
+        <li>
+          <a href="/socials" class="icon"> <img src="/svgs/nav/socials.svg" width="28" alt="socials" /> </a>
+        </li>
+        <li>
+          <a href="/upcoming" class="icon"> <img src="/svgs/nav/upcoming.svg" width="28" alt="upcoming" /> </a>
+        </li>
+      {:else}
+        <li class="text"><a href="/api">API</a></li>
+        <li class="text"><a href="/downloads">Downloads</a></li>
+        <li class="text"><a href="/portfolio">Portfolio</a></li>
+        <li class="text"><a href="/projects">Projects</a></li>
+        <li class="text"><a href="/socials">Socials</a></li>
+        <li class="text"><a href="/upcoming">Upcoming</a></li>
+      {/if}
     </ul>
   </div>
 </nav>
 
 <slot />
 
+{#if lastUpdated && lastMessage}
+  <footer class="update">
+    <a href="https://github.com/ShermanZero/shermanzero.com" target="_">LATEST BUILD</a> ON [{lastUpdated}] -
+    <q>{lastMessage}</q>
+  </footer>
+{/if}
+
 <style>
   @media only screen and (max-width: 800px) {
-    li {
+    .text {
       display: none;
     }
+
+    .toggle {
+      display: none;
+    }
+  }
+  .wrapper {
+    width: 100%;
+    height: 100%;
   }
 
   .update {
@@ -84,7 +115,7 @@
     color: rgb(160, 72, 167);
   }
 
-  span:not(#link) {
+  span {
     position: absolute;
     display: block;
     text-align: center;
@@ -95,13 +126,6 @@
     color: white;
 
     font-family: "Raleway", sans-serif;
-  }
-
-  #link {
-    margin-left: -5px;
-    padding-left: 0;
-    color: rgb(105, 105, 105);
-    font-style: italic;
   }
 
   span a {
@@ -140,26 +164,41 @@
 
   li {
     flex-shrink: 1;
-    align-self: flex-end;
+    align-self: center;
 
     list-style-type: none;
     margin: 0;
 
+    border-radius: 4px;
+
+    padding: 0px 4px;
+
     transition: all 0.1s;
+    overflow: hidden;
   }
 
   li:last-child {
-    padding-right: 25px;
+    margin-right: 25px;
   }
 
   li:hover {
     background-color: white;
   }
 
+  .icon {
+    transition: all 0.12s;
+  }
+
+  .icon:hover {
+    filter: invert(80%);
+  }
+
   li:hover * {
     font-family: "Raleway", sans-serif;
     color: black;
     font-weight: 800;
+
+    cursor: pointer;
   }
 
   li a {
@@ -169,10 +208,11 @@
     text-decoration: none;
     text-transform: uppercase;
 
+    font-size: 0.8em;
+
     width: 100%;
     height: 100%;
 
-    padding: 12px 10px;
     transition: all 0.12s;
 
     text-shadow: 0px 5px 5px rgb(20, 20, 20, 0.3);
@@ -180,6 +220,81 @@
 
   li a:hover {
     cursor: pointer;
-    padding: 12px 36px;
+    padding: 12px 18px;
+  }
+
+  /* The switch - the box around the slider */
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    height: 24px;
+
+    margin: 0;
+    margin-right: 6px;
+    padding: 0;
+    transform: translateY(0px);
+  }
+
+  /* Hide default HTML checkbox */
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+
+    margin: 0;
+    padding: 0;
+
+    transform: translateY(-5px);
+  }
+
+  /* The slider */
+  .slider {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.1s;
+    transition: 0.1s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 16px;
+    width: 16px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.1s;
+    transition: 0.1s;
+  }
+
+  input:checked + .slider {
+    background-color: #2196f3;
+  }
+
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(16px);
+    -ms-transform: translateX(16px);
+    transform: translateX(16px);
+  }
+
+  a {
+    padding: 0;
+  }
+
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
   }
 </style>
---
+++ src/routes/invitation.svelte
@@ -0,0 +1,119 @@
+<script>
+  const onFocus = (e) => {
+    e.target.select();
+  };
+
+  const submit = (e) => {};
+</script>
+
+<div>
+  <h3>Invite Code</h3>
+
+  <ul>
+    <li><input id="in1" type="text" required maxlength="1" on:focus={onFocus} /></li>
+    <li><input id="in2" type="text" required maxlength="1" on:focus={onFocus} /></li>
+    <li><input id="in3" type="text" required maxlength="1" on:focus={onFocus} /></li>
+    <li><input id="in4" type="text" required maxlength="1" on:focus={onFocus} /></li>
+  </ul>
+
+  <button on:click={submit}>SUBMIT</button>
+</div>
+
+<style>
+  h3 {
+    text-transform: uppercase;
+    font-family: "Raleway", sans-serif;
+    font-weight: 800;
+
+    text-shadow: 0px 0px 1px #000;
+  }
+
+  div {
+    width: 100%;
+    margin-top: 80px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    border: none;
+  }
+
+  li {
+    margin: 5px;
+
+    border: none;
+  }
+
+  input {
+    width: 60px;
+    height: 90px;
+    border: none;
+    border-radius: 12px;
+    outline: none;
+    background: transparent;
+
+    transition: all 0.12s;
+
+    font-family: "Roboto Condensed", sans-serif;
+    font-size: 3em;
+    font-weight: 600;
+    text-align: center;
+    vertical-align: text-top;
+
+    text-transform: uppercase;
+    caret-color: transparent;
+
+    background-color: rgb(94, 94, 94);
+    box-shadow: 0 5px 0px 0px rgb(63, 63, 63);
+  }
+
+  input::selection {
+    background: transparent;
+  }
+
+  input:focus {
+    background-color: rgb(177, 177, 177);
+  }
+
+  input:valid:not(:focus) {
+    background-color: white;
+  }
+
+  button {
+    margin-top: 25px;
+    width: 280px;
+
+    padding: 12px;
+    border-radius: 12px;
+
+    font-family: "Raleway", sans-serif;
+    font-weight: 800;
+    font-size: 1.25em;
+
+    box-shadow: 0 5px 0px 0px rgb(63, 63, 63);
+    outline: none;
+    border: none;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  button:active {
+    background-color: rgb(177, 177, 177);
+
+    transform: translateY(5px);
+    box-shadow: none;
+  }
+</style>
